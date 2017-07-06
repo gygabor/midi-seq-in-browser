029d5b4d63d1bfc6a0b82c409ced5b328294d649
gygabor <gyorgyg@gmail.com>
2017-07-06 18:10:05 +0200

    feat(midi-seq.js)play loop with one note

--- midi-seq.js
+++ midi-seq.js
@@ -1,7 +1,26 @@
 'use strict';
 
-const triggerButton = document.querySelector('button');
+const playButton = document.querySelector('button');
 
-triggerButton.addEventListener('click', function(e) {
-    console.log('trig');
+WebMidi.enable((err) => {
+    if (err) {
+    console.log("WebMidi could not be enabled.", err);
+    } else {
+        const output =  WebMidi.getOutputByName('USB Midi Cable MIDI 1');
+        playButton.addEventListener('click', (e) =>{
+            sequenceHandling(output, start);
+        });
+    }
 });
+
+const playOneNote = function(output) {
+    const options = {
+        time: 50
+    }
+    output.playNote('C3', 1);
+    output.stopNote('C3', 1, options)
+}
+
+const sequenceHandling = function(output) {
+    const playLoop = setInterval(playOneNote, 1000/8, output);
+}
